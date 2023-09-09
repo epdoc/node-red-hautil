f68cce4f9b10e061bd5f3a592ea5d381a371635e
Jim Pravetz <jpravetz@gmail.com>
2023-09-09 17:22:49 -0600

    HA tests

--- src/ha.js
+++ src/ha.js
@@ -1,72 +1,73 @@
-(function () {
-  require('utils');
-  require('fan');
+import { Fan } from './fan';
+import { isFunction } from './util';
 
-  let HA = function () {
-    function HA(ha, options) {
-      this.ha = ha.homeAssistant;
-      this.options = options || {};
-      this.warn = isFunction(this.options.warn) ? this.options.warn : null;
+export class HA {
+  ha;
+  options;
+  log;
+
+  constructor(globalHomeAssistant, options) {
+    this.ha = globalHomeAssistant.homeAssistant;
+    this.options = options || {};
+    this.log = isFunction(this.options.log) ? this.options.log : null;
+  }
+
+  isEntityOn(entity_id) {
+    const entity = this.ha.states[entity_id];
+    return entity && entity.state === 'on' ? true : false;
+  }
+
+  /**
+   *
+   * @param {Obj} sensorDict Object containing an id property which is an entity_id.
+   */
+  retrieveSensorsData(sensorDict) {
+    for (const name in sensorDict) {
+      if (sensorDict.hasOwnProperty(name)) {
+        let item = sensorDict[name];
+        item.obj = this.ha.states[item.id];
+        if (item.obj) {
+          item.state = item.obj.state;
+        }
+      }
     }
+  }
 
-    HA.prototype.isEntityOn = function (entity_id) {
-      const entity = this.ha.states[entity_id];
-      return entity && entity.state === 'on' ? true : false;
+  /**
+   * d.service = on, off, toggle, speed, percentage, arm_night, arm_away, arm_home, disarm
+   */
+  static getServicePayload(params) {
+    let p = {
+      service: params.service,
+      target: {
+        entity_id: params.entity_id,
+      },
     };
-
-    /**
-     * d.service = on, off, toggle, speed, percentage, arm_night, arm_away, arm_home, disarm
-     */
-    HA.prototype.getServicePayload = function (params) {
-      let p = {
-        service: params.service,
-        target: {
-          entity_id: params.entity_id,
-        },
-      };
-      if (params.domain) {
-        p.domain = params.domain;
-      } else {
-        const parts = params.entity_id.split('.');
-        p.domain = parts[0];
+    if (params.domain) {
+      p.domain = params.domain;
+    } else {
+      const parts = params.entity_id.split('.');
+      p.domain = parts[0];
+    }
+    if (params.service === 'on' || params.service === 'off') {
+      p.service = 'turn_' + params.service;
+    }
+    if (p.domain === 'fan') {
+      if (params.service === 'speed') {
+        p.service = 'set_percentage';
+        p.data = {
+          percentage: Fan.speedToPercentage(params.speed),
+        };
+      } else if (p.service === 'percentage') {
+        p.data = {
+          percentage: params.percentage,
+        };
       }
-      if (params.service === 'on' || params.service === 'off') {
-        p.service = 'turn_' + d.service;
-      }
-      if (params.domain === 'fan') {
-        let fan = new Fan();
-        if (params.service === 'speed') {
-          p.service = 'set_percentage';
-          p.data = {
-            percentage: fan.speedToPercentage(params.speed),
-          };
-        } else if (params.service === 'percentage') {
-          p.data = {
-            percentage: params.percentage,
-          };
-        }
-      } else if (params.domain === 'cover') {
-        p.service = params.service + '_cover';
-      } else if (params.domain === 'alarm_control_panel') {
-        p.service = 'alarm_' + params.service;
-      }
-      return p;
-    };
-
-    /**
-     *
-     * @param {Obj} sensorDict Object containing an id property which is an entity_id.
-     */
-    HA.prototype.retrieveSensorsData = function (sensorDict) {
-      for (const name in sensorDict) {
-        if (sensorDict.hasOwnProperty(name)) {
-          let item = sensorDict[name];
-          item.obj = this.ha.states[item.id];
-          if (item.obj) {
-            item.state = item.obj.state;
-          }
-        }
-      }
-    };
-  };
-});
+    } else if (p.domain === 'cover') {
+      p.service = params.service + '_cover';
+    } else if (p.domain === 'alarm_control_panel') {
+      p.service = 'alarm_' + params.service;
+    }
+    return p;
+  }
+}
---
+++ test/ha.test.js
@@ -0,0 +1,67 @@
+import { HA } from '../src/ha.js';
+import { isObject } from '../src/util.js';
+
+describe('ha', () => {
+  describe('entity data', () => {
+    const gHA = {
+      homeAssistant: {
+        states: {
+          entity2: {
+            state: 'on',
+          },
+          entity1: {
+            state: 'off',
+          },
+        },
+      },
+    };
+    let ha = new HA(gHA);
+
+    it('isEntityOn', () => {
+      expect(ha.isEntityOn('entity2')).toEqual(true);
+      expect(ha.isEntityOn('entity1')).toEqual(false);
+    });
+    it('retrieve sensor data', () => {
+      const dict = {
+        e1: { id: 'entity1' },
+        e2: { id: 'entity2' },
+      };
+      ha.retrieveSensorsData(dict);
+      expect(isObject(dict.e1.obj)).toEqual(true);
+      expect(isObject(dict.e2.obj)).toEqual(true);
+      expect(dict.e2.state).toEqual('on');
+      expect(dict.e1.state).toEqual('off');
+    });
+  });
+
+  describe('service payload', () => {
+    it('light on', () => {
+      const params = {
+        service: 'on',
+        entity_id: 'light.entity3',
+      };
+      const p = HA.getServicePayload(params);
+      expect(isObject(p)).toEqual(true);
+      expect(isObject(p.target)).toEqual(true);
+      expect(p.service).toEqual('turn_on');
+      expect(p.target.entity_id).toEqual(params.entity_id);
+      expect(p.domain).toEqual('light');
+    });
+    it('fan speed', () => {
+      const params = {
+        service: 'speed',
+        entity_id: 'fan.entity4',
+        speed: 3,
+      };
+      const p = HA.getServicePayload(params);
+      expect(isObject(p)).toEqual(true);
+      expect(isObject(p.target)).toEqual(true);
+      expect(p.target.entity_id).toEqual('fan.entity4');
+      expect(isObject(p.data)).toEqual(true);
+      expect(p.service).toEqual('set_percentage');
+      expect(p.target.entity_id).toEqual(params.entity_id);
+      expect(p.domain).toEqual('fan');
+      expect(p.data.percentage).toEqual(50);
+    });
+  });
+});
