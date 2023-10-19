04eb83344912778df9145658ae7f07ca64ee0da5
Jim Pravetz <jpravetz@gmail.com>
2023-10-19 11:52:02 -0600

    Add service builder shortcuts

--- src/service.js
+++ src/service.js
@@ -1,4 +1,33 @@
-import { isDict, isFunction } from 'epdoc-util';
+import { isDict, isFunction, isNonEmptyString } from 'epdoc-util';
+
+const REG = {
+  cover: {
+    postfix: {
+      cover: new RegExp(/^(open|close|stop)$/),
+    },
+  },
+  alarm_control_panel: {
+    prefix: {
+      arm: new RegExp(/^(arm_away|arm_home|arm_night|arm_vacation|disarm|trigger|arm_custom_bypass)$/),
+    },
+  },
+  fan: {
+    prefix: {
+      turn: new RegExp(/^(on|off)$/),
+      set: new RegExp(/^(direction|percentage|preset_mode)$/),
+    },
+  },
+  light: {
+    prefix: {
+      turn: new RegExp(/^(on|off)$/),
+    },
+  },
+  switch: {
+    prefix: {
+      turn: new RegExp(/^(on|off)$/),
+    },
+  },
+};
 
 export function newService(name, options) {
   return new Service(name, options);
@@ -6,6 +35,10 @@ export function newService(name, options) {
 export function newFan(name, options) {
   return new Service('fan.' + name, options);
 }
+
+/**
+ * Payload builder for service call
+ */
 export class Service {
   _entity_id;
   _warn;
@@ -32,29 +65,96 @@ export class Service {
     return this._payload.target.entity_id;
   }
 
+  /**
+   * Call if you wish to override the domain that is automatically determined
+   * from the entity_id.
+   * @param {*} val
+   * @returns
+   */
   domain(val) {
     this._payload.domain = val;
     return this;
   }
 
+  /**
+   * Shortcut to set service to turn_on.
+   * @returns
+   */
   on() {
     return this.service('on');
   }
 
+  /**
+   * Shortcut to set service to turn_off.
+   * @returns
+   */
   off() {
     return this.service('off');
   }
 
+  /**
+   * Shortcut to set service to close_cover.
+   * @returns
+   */
+  close() {
+    return this.service('close');
+  }
+
+  /**
+   * Shortcut to set service to open_cover.
+   * @returns
+   */
+  open() {
+    return this.service('open');
+  }
+
+  /**
+   * Shortcut to set service to alarm_disarm.
+   * @returns
+   */
+  disarm() {
+    return this.service('disarm');
+  }
+
+  /**
+   * Set the service. Takes the following shortcuts:
+   *  - for most services on and off result in turn_on and turn_off
+   *  - for cover, takes open, close, stop
+   * @param {} val
+   * @returns
+   */
   service(val) {
-    if (this._payload.domain === 'cover') {
-      this._payload.service = val + '_cover';
-    } else if (this._payload.domain === 'alarm_control_panel') {
-      this._payload.service = 'alarm_' + val;
-    } else if (val === 'on' || val === 'off') {
-      this._payload.service = 'turn_' + val;
-    } else {
-      this._payload.service = val;
+    if (isNonEmptyString(val)) {
+      Object.keys(REG).forEach((domain) => {
+        if (this._payload.domain === domain) {
+          if (REG[domain].prefix) {
+            Object.keys(REG[domain].prefix).forEach((prefix) => {
+              if (REG[domain].prefix[prefix].test(val)) {
+                this._payload.service = prefix + '_' + val;
+              }
+            });
+          }
+          if (REG[domain].postfix) {
+            Object.keys(REG[domain].postfix).forEach((postfix) => {
+              if (REG[domain].postfix[postfix].test(val)) {
+                this._payload.service = val + '_' + postfix;
+              }
+            });
+          }
+        }
+      });
+      if (!this._payload.service) {
+        this._payload.service = val;
+      }
     }
+    return this;
+  }
+
+  date(val) {
+    this._payload.service = 'set_datetime';
+    this._payload.data = {
+      timestamp: Math.round(val.getTime() / 1000),
+    };
     return this;
   }
 
--- test/service.test.js
+++ test/service.test.js
@@ -61,6 +61,114 @@ describe('service', () => {
           },
         });
       });
+      describe('light', () => {
+        it('on', () => {
+          const light = new Service('light.master_bedroom');
+          let result = light.on().payload();
+          expect(isObject(result)).toEqual(true);
+          expect(result).toEqual({
+            target: { entity_id: 'light.master_bedroom' },
+            domain: 'light',
+            service: 'turn_on',
+          });
+        });
+        it('off', () => {
+          const light = new Service('light.master_bedroom');
+          let result = light.domain('light').off().payload();
+          expect(isObject(result)).toEqual(true);
+          expect(result).toEqual({
+            target: { entity_id: 'light.master_bedroom' },
+            domain: 'light',
+            service: 'turn_off',
+          });
+        });
+      });
+      describe('switch', () => {
+        it('on', () => {
+          const payload = new Service('switch.master_bedroom');
+          let result = payload.on().payload();
+          expect(isObject(result)).toEqual(true);
+          expect(result).toEqual({
+            target: { entity_id: 'switch.master_bedroom' },
+            domain: 'switch',
+            service: 'turn_on',
+          });
+        });
+        it('off', () => {
+          const payload = new Service('switch.master_bedroom');
+          let result = payload.domain('switch').off().payload();
+          expect(isObject(result)).toEqual(true);
+          expect(result).toEqual({
+            target: { entity_id: 'switch.master_bedroom' },
+            domain: 'switch',
+            service: 'turn_off',
+          });
+        });
+      });
+      describe('cover', () => {
+        it('close', () => {
+          const payload = new Service('cover.master_bedroom');
+          let result = payload.close().payload();
+          expect(isObject(result)).toEqual(true);
+          expect(result).toEqual({
+            target: { entity_id: 'cover.master_bedroom' },
+            domain: 'cover',
+            service: 'close_cover',
+          });
+        });
+        it('open', () => {
+          const payload = new Service('cover.master_bedroom');
+          let result = payload.service('open').payload();
+          expect(isObject(result)).toEqual(true);
+          expect(result).toEqual({
+            target: { entity_id: 'cover.master_bedroom' },
+            domain: 'cover',
+            service: 'open_cover',
+          });
+        });
+        it('stop', () => {
+          const payload = new Service('cover.master_bedroom');
+          let result = payload.service('stop_cover').payload();
+          expect(isObject(result)).toEqual(true);
+          expect(result).toEqual({
+            target: { entity_id: 'cover.master_bedroom' },
+            domain: 'cover',
+            service: 'stop_cover',
+          });
+        });
+      });
+      describe('alarm_control_panel', () => {
+        it('close', () => {
+          const payload = new Service('alarm_control_panel.workshop');
+          let result = payload.disarm().payload();
+          expect(isObject(result)).toEqual(true);
+          expect(result).toEqual({
+            target: { entity_id: 'alarm_control_panel.workshop' },
+            domain: 'alarm_control_panel',
+            service: 'arm_disarm',
+          });
+        });
+        it('open', () => {
+          const payload = new Service('alarm_control_panel.workshop');
+          let result = payload.service('arm_away').payload();
+          expect(isObject(result)).toEqual(true);
+          expect(result).toEqual({
+            target: { entity_id: 'alarm_control_panel.workshop' },
+            domain: 'alarm_control_panel',
+            service: 'arm_arm_away',
+          });
+        });
+        it('stop', () => {
+          const payload = new Service('alarm_control_panel.workshop');
+          let result = payload.service('arm_trigger').payload();
+          expect(isObject(result)).toEqual(true);
+          expect(result).toEqual({
+            target: { entity_id: 'alarm_control_panel.workshop' },
+            domain: 'alarm_control_panel',
+            service: 'arm_trigger',
+          });
+        });
+      });
     });
   });
 });
